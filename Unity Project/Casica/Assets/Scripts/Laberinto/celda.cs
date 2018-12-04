ca9b04fb0af623786bfeb2746a0b69397aaeb42f
AcroSenpai <aitor.perez.muro@gmail.com>
2018-12-04 19:12:24 +0100

    cosas

---
+++ Unity Project/Casica/Assets/Scripts/Laberinto/celda.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class celda : Interactive
+{
+    int num;
+    GameObject suelo;
+    PlayerController pc;
+
+    public override void Activar()
+    {
+        Debug.Log("Activo esto que es maravilloso");
+        if(num != 2)
+        {
+            num++;
+        }
+        else
+        {
+
+        }
+               
+    }
+}
