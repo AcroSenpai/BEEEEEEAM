32a813fed6295d1555e8841bdfb68493d951529a
MrJake7X <alexreverter7@gmail.com>
2018-12-11 17:13:42 +0100

    focus hoja niño

---
+++ Unity Project/Casica/Assets/Scripts/TriggerS/TriggerHoja.cs
@@ -0,0 +1,56 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHoja : MonoBehaviour {
+
+    public CinemachineVirtualCamera camHoja;
+
+    private bool zoom;
+
+    private bool cam;
+
+    private void Start()
+    {
+        camHoja.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(zoom)
+        {
+            if (Input.GetButtonDown("Fire1") && !cam)
+            {
+                cam = true;
+            }
+            else if (Input.GetButtonDown("Fire1") && cam)
+            {
+                cam = false;
+            }
+        }
+        else
+        {
+            cam = false;
+        }
+
+        camHoja.enabled = cam;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            zoom = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            cam = false;
+            zoom = false;
+        }
+    }
+}
