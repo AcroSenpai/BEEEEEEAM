169e7b8e9a2ddd2a73c40b8cf57cfcd355f9397c
MrJake7X <alexreverter7@gmail.com>
2018-12-19 18:57:19 +0100

    nfgh

--- Unity Project/Casica/Assets/Scripts/UI/Logo.cs
+++ Unity Project/Casica/Assets/Scripts/UI/Logo.cs
@@ -8,16 +8,31 @@ public class Logo : MonoBehaviour {
 
     private VideoPlayer video;
 
+    private float timeCounter;
+
+    private bool go;
+
 	void Start ()
     {
         video = GetComponent<VideoPlayer>();
 	}
-	
-	void Update ()
+
+    void Update()
     {
-		if(!video.isPlaying)
+        if (timeCounter >= 3 && !video.isPlaying && !go)
+        {
+            video.Play();
+            timeCounter = 0;
+            go = true;
+        }
+        else
+        {
+            timeCounter += 1 * Time.deltaTime;
+        }
+
+        if (!video.isPlaying && go)
         {
             SceneManager.LoadScene("MenuPrincipal");
         }
-	}
+    }
 }
