3f42eb9143f0d1196a44e20e8e331df6d7a5ac34
MrJake7X <alexreverter7@gmail.com>
2019-02-04 17:04:10 +0100

    camara llave

---
+++ Unity Project/Casica/Assets/Scripts/TriggerS/TriggerLlave.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class TriggerLlave : MonoBehaviour
+{
+    public CinemachineVirtualCamera camZoom;
+
+    private void Start()
+    {
+        camZoom = GetComponentInChildren<CinemachineVirtualCamera>();
+        camZoom.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            camZoom.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            camZoom.enabled = false;
+        }
+    }
+}
