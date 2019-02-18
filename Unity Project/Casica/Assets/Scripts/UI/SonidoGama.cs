ad9c2e749937ff95b225eb33377ae61fba6c6583
buebas <sadsadasdasd@gmail.com>
2019-02-18 17:09:24 +0100

    sonidicos

---
+++ Unity Project/Casica/Assets/Scripts/UI/SonidoGama.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SonidoGama : MonoBehaviour
+{
+    public AudioSource sonidico;
+    public Slider gama;
+    public float valAnterior;
+    public float tiempo;
+    public float espera;
+    public bool sonar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = 0;
+        sonar = false;
+    }
+
+    void Update()
+    {      
+        if(valAnterior == gama.value && sonar)
+        {
+            if(tiempo > espera)
+            {
+                gamasonido();
+                tiempo = 0;
+                sonar = false;
+            }
+            else
+            {
+                tiempo += Time.deltaTime;
+            }
+        }
+        
+    }
+
+    public void gamasonido()
+    {
+        sonidico.Play();
+    }
+
+    public void SetGamaValue()
+    {
+        valAnterior = gama.value;
+        sonar = true;
+    }
+}
