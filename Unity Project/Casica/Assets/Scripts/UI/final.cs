da33ed059b8c60c29c4cce2b87940c736920c978
AcroSenpai <aitor.perez.muro@gmail.com>
2019-04-25 20:54:55 +0200

    frfrfrfrfr

---
+++ Unity Project/Casica/Assets/Scripts/UI/final.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class final : MonoBehaviour
+{
+    public void CargarMenuPrincipal()
+    {
+        SceneManager.LoadScene("1MenuPrincipal");
+    }
+}
